efc9b902ac07cd48002d6fa63339e58bddbafe85
Alivin <wangyongjie@mediinfo.com.cn>
2025-11-13 10:54:19 +0800

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://www.kaiyuanshe.top' : undefined,
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://www.kaiyuanshe.cn' : undefined,
   eslint: {
     ignoreDuringBuilds: true, // 忽略 eslint 检查
   },
