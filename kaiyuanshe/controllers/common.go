db4407257d6ec34b978b683c38cf02e9845fb797
phoouze <smallfu666@gmail.com>
2025-11-23 20:05:35 +0800

    fix: update register required

--- kaiyuanshe/controllers/common.go
+++ kaiyuanshe/controllers/common.go
@@ -57,9 +57,9 @@ type UpdateEventPublishStatusRequest struct {
 
 // register
 type RegisterRequest struct {
-	Username string `json:"username" binding:"required,min=3,max=20"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6,max=32"`
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 // login
