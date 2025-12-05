4ad270377bcf64a72bb420686b5d4ee0ff1e3d08
phoouze <smallfu666@gmail.com>
2025-12-05 21:56:58 +0800

    fix: update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true, // 忽略 eslint 检查
-  },
   typescript: {
     ignoreBuildErrors: true, // 忽略 TypeScript 检查
   },
